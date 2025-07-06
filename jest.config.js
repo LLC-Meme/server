a3957da3e780bdaa12fbf9b87e94845ee5a354dd
J-Naish <mjyrgd2332@i.softbank.jp>
2025-07-06 20:14:27 +0900

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+  ],
+  testTimeout: 60000, // 60 seconds for puppeteer tests
+};
